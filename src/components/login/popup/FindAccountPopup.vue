f202e202f770c82cef212e97b791bbf303373633
LEE YEJIN <dldulwls@gmail.com>
2023-10-20 15:40:15 +0900

    로그인&계정찾기 모달 진행중

--- src/components/login/popup/FindAccountPopup.vue
+++ src/components/login/popup/FindAccountPopup.vue
@@ -1,3 +1,169 @@
-<script setup></script>
-<template>계정 찾기</template>
-<style scoped lang="scss"></style>
+<script setup>
+import { computed, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import DefaultModal from "@/components/layout/DefaultModal.vue";
+import { BaseInputSubmit, BaseButton } from "@/components/base";
+import ChangePwPopup from "@/components/login/popup/ChangePwPopup.vue";
+
+const emit = defineEmits(["close"]);
+
+const router = useRouter();
+const route = useRoute();
+
+const { routePath, gotoPage } = routeHandler();
+const { changePwPopup, changePwPopupHandler } = changePwControl();
+
+function routeHandler() {
+  const routePath = computed(() => {
+    return route.path.split("/")[1];
+  });
+
+  function gotoPage(pageName) {
+    router.push({ name: pageName });
+  }
+  return { routePath, gotoPage };
+}
+
+function changePwControl() {
+  const changePwPopup = ref(false);
+
+  function changePwPopupHandler() {
+    changePwPopup.value = !changePwPopup.value;
+  }
+  return { changePwPopup, changePwPopupHandler };
+}
+</script>
+<template>
+  <!-- <DefaultModal :width="440" :height="420" @close="emit('close')">
+    <template #contents>
+      <div class="container">
+        <h1 class="container--title">내 계정 찾기</h1>
+        <div class="info">
+          <span class="info--text"
+            >본인 인증 시 메세지를 통해 임시 비밀번호가 발급됩니다.</span
+          >
+          <span class="info--text"
+            >로그인 후 마이페이지에서 새로운 비밀번호로 변경 바랍니다.</span
+          >
+        </div>
+        <div class="container__inputform">
+          <BaseInputSubmit
+            type="password"
+            :label="'전화번호'"
+            :placeholder="'01012345678'"
+            :width="360"
+            :btnLabel="'인증번호 전송'"
+          />
+          <BaseInputSubmit
+            type="password"
+            :width="360"
+            :placeholder="'인증번호 입력'"
+          />
+        </div>
+        <div class="account-container">
+          <span
+            class="link"
+            :class="{ action: routePath === 'signup' }"
+            @click="
+              () => {
+                emit('close');
+                gotoPage('signup');
+              }
+            "
+            >회원가입</span
+          >
+          <span class="vertical-bar"> &#124; </span>
+          <span class="link"> 뒤로 가기 </span>
+        </div>
+      </div>
+    </template>
+  </DefaultModal> -->
+
+  <!-- 내 계정 찾기(아이디 찾기 완료) -->
+  <DefaultModal :width="440" :height="420" @close="emit('close')">
+    <template #contents>
+      <div class="container">
+        <h1 class="container--title">내 계정 찾기</h1>
+        <div class="info">
+          <span class="info--text"
+            >010-0000-0000와 일치하는 계정은 아래와 같습니다.
+          </span>
+          <span class="info--text">
+            비밀번호를 분실한 경우 계정과 동일한 번호로 임시 비밀번호 를 발급해
+            드립니다.
+          </span>
+        </div>
+        <div class="user-card">유저 아이디</div>
+        <BaseButton class="pink" :width="360">로그인 하기</BaseButton>
+        <BaseButton class="pink" :width="360" @click="changePwPopupHandler"
+          >새 비밀번호로 변경하기</BaseButton
+        >
+      </div>
+      <template v-if="changePwPopup">
+        <ChangePwPopup @close="changePwPopupHandler" />
+      </template>
+    </template>
+  </DefaultModal>
+</template>
+<style scoped lang="scss">
+@mixin flex {
+  display: flex;
+  flex-direction: column;
+}
+
+.container {
+  @include flex;
+  align-items: center;
+  padding: 20px;
+
+  &--title {
+    font-weight: 500;
+    font-size: 24px;
+    margin: 0 0 20px 0;
+  }
+  &__inputform {
+    @include flex;
+    row-gap: 10px;
+  }
+}
+
+.info {
+  @include flex;
+  align-items: center;
+  margin-bottom: 24px;
+  &--text {
+    font-size: 14px;
+    font-weight: 400;
+    color: #696363;
+    margin: 0 2px 0 2px;
+  }
+}
+
+.account-container {
+  margin-top: 80px;
+  & span {
+    font-size: 14px;
+    font-weight: 300;
+    color: #696363;
+    margin: 0 2px 0 2px;
+  }
+
+  &__terms {
+    .link {
+      cursor: pointer;
+    }
+    .vertical-bar {
+      color: var(--border-gray);
+    }
+  }
+}
+
+/* 왜 안 먹히지 */
+.user-card {
+  width: 360;
+  height: 60;
+  border: solid 1px var(--border-gray);
+}
+.button-container {
+}
+</style>
--- src/components/login/popup/LoginPopup.vue
+++ src/components/login/popup/LoginPopup.vue
@@ -1,11 +1,183 @@
 <script setup>
+import { computed, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import DefaultModal from "@/components/layout/DefaultModal.vue";
+import { BaseButton, BaseInput } from "@/components/base";
+import kakao from "@/assets/login/kakao.svg";
+import google from "@/assets/login/google.svg";
+import FindAccountPopup from "@/components/login/popup/FindAccountPopup.vue";
+
+const router = useRouter();
+const route = useRoute();
 
 const emit = defineEmits(["close"]);
+
+const { routePath, gotoPage } = routeHandler();
+const { findAccountPopup, findAccountPopupHandler } = findAccountControl();
+
+function routeHandler() {
+  const routePath = computed(() => {
+    return route.path.split("/")[1];
+  });
+
+  function gotoPage(pageName) {
+    router.push({ name: pageName });
+  }
+  return { routePath, gotoPage };
+}
+function findAccountControl() {
+  const findAccountPopup = ref(false);
+
+  function findAccountPopupHandler() {
+    console.log("계정 뜨니");
+    findAccountPopup.value = !findAccountPopup.value;
+    if (findAccountPopup.value) {
+      //여기서 로그인 모달을 닫아야 할 것 같은데
+      console.log("어떻게 닫지");
+    }
+  }
+  return { findAccountPopup, findAccountPopupHandler };
+}
 </script>
 <template>
   <DefaultModal :width="440" :height="510" @close="emit('close')">
-    <template #contents> </template>
+    <template #contents>
+      <div class="container">
+        <h1 class="container--title">로그인</h1>
+        <div class="container__inputform">
+          <BaseInput
+            :placeholder="'아이디를 입력하세요.'"
+            :label="'아이디'"
+            :width="360"
+          />
+          <BaseInput
+            :placeholder="'비밀번호를 입력하세요.'"
+            :label="'비밀번호'"
+            :width="360"
+            type="password"
+          />
+          <!-- <span class="error-message">존재하지 않는 아이디입니다.</span> -->
+        </div>
+        <div class="login-container">
+          <BaseButton class="pink" :width="360">로그인</BaseButton>
+          <div class="login-container__terms">
+            <span
+              class="link"
+              :class="{ action: routePath === 'signup' }"
+              @click="
+                () => {
+                  emit('close');
+                  gotoPage('signup');
+                }
+              "
+              >회원가입</span
+            >
+            <span class="vertical-bar"> &#124; </span>
+            <span class="link" @click="findAccountPopupHandler">
+              아이디/비밀번호 찾기
+            </span>
+          </div>
+        </div>
+        <div class="footer">
+          <div class="footer--img">
+            <div class="kakao-container">
+              <img :src="kakao" />
+            </div>
+            <div class="google-container">
+              <img :src="google" />
+            </div>
+          </div>
+          <span>연동된 소셜 계정으로 로그인</span>
+        </div>
+      </div>
+      <template v-if="findAccountPopup">
+        <FindAccountPopup @close="findAccountPopupHandler" />
+      </template>
+    </template>
   </DefaultModal>
 </template>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@mixin flex {
+  display: flex;
+  flex-direction: column;
+}
+
+button {
+  all: unset;
+}
+
+.container {
+  @include flex;
+  align-items: center;
+  padding: 20px;
+
+  &--title {
+    font-weight: 500;
+    font-size: 24px;
+    margin: 0 0 20px 0;
+  }
+  &__inputform {
+    @include flex;
+    row-gap: 10px;
+  }
+}
+
+.login-container {
+  @include flex;
+  align-items: center;
+  row-gap: 10px;
+  margin-top: 36px;
+  & span {
+    font-size: 14px;
+    font-weight: 300;
+    color: #696363;
+    margin: 0 2px 0 2px;
+  }
+
+  &__terms {
+    .link {
+      cursor: pointer;
+    }
+    .vertical-bar {
+      color: var(--border-gray);
+    }
+  }
+}
+
+.footer {
+  @include flex;
+  align-items: center;
+  margin-top: 30px;
+  & span {
+    font-size: 14px;
+    font-weight: 300;
+    color: #696363;
+  }
+
+  &--img {
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+    margin-bottom: 20px;
+    cursor: pointer;
+  }
+}
+.kakao-container {
+  @include flex;
+  align-items: center;
+  justify-content: center;
+  width: 60px;
+  height: 60px;
+  background-color: #ffe747;
+  border-radius: 8px;
+}
+.google-container {
+  @include flex;
+  align-items: center;
+  justify-content: center;
+  width: 60px;
+  height: 60px;
+  border: solid 1px var(--border-gray);
+  border-radius: 8px;
+}
+</style>
